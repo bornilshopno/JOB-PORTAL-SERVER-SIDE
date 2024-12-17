5d85c4892588f48d3a24282c73d8a128b89ad5b2
Ashraf Manna <optimisticashraf@gmail.com>
2024-12-18 00:28:56 +0600

    dynamic sections added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port= process.env.PORT || 5000
 
@@ -32,11 +32,61 @@ async function run() {
     await client.connect();
 
 const jobCollection=client.db("jobDB").collection("jobs")
+const applicationCollection=client.db("jobDB").collection("applications")
 
 app.get("/jobs", async(req,res)=>{
     const cursor=jobCollection.find()
     const result=await cursor.toArray()
     res.send(result)
+})
+
+app.post("/job-applications", async(req,res)=>{
+  const application=req.body;
+  const result= await applicationCollection.insertOne(application);
+  res.send(result)
+})
+
+app.get("/job-applications", async(req,res)=>{
+  const cursor=applicationCollection.find();
+  const result= await cursor.toArray();
+  res.send(result);
+})
+
+app.delete("/job-applications/:id", async(req,res)=>{
+const id=req.params.id;
+const query={_id: new ObjectId(id)}
+const result=await applicationCollection.deleteOne(query);
+res.send(result)
+
+})
+
+app.get("/job-application", async(req,res)=>{
+  const email=req.query.email;
+  const query={applicant_email:email};
+  const result=await applicationCollection.find(query).toArray();
+
+  //fokira way to get data from other collection of this database DB
+for (const apply of result){
+  const query1={_id: new ObjectId(apply.job_id)};
+  const job=await jobCollection.findOne(query1);
+
+  if(job){
+    apply.title=job.title;
+    apply.jobType=job.jobType;
+    apply.company=job.company;
+    apply.company_logo=job.company_logo;
+    apply.location=job.location;
+    apply.description=job.description;
+  }
+}
+  res.send(result)
+})
+
+app.get("/jobs/:id", async(req,res)=>{
+  const id=req.params.id;
+  const query={_id: new ObjectId(id)};
+  const result=await jobCollection.findOne(query);
+  res.send(result)
 })
 
 
